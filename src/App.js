a11893bfbb66e3a474c5ff083bb95a003877a0b9
Lokesh <lokeshsingh7695@gmail.com>
2024-03-11 16:57:59 +0530

    Calculator App using React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+
+
+const App = () => {
+  // useState hook
+  const [val, setVal] = useState("0");
+
+  const setData = (data) => {
+    if (data === "=") {
+      const result = eval(val);
+      setVal(result.toString());
+    } else if (data === "C") {
+      setVal("0");
+    } else {
+      if (val === "0") setVal(data.toString());
+      else setVal(val + data);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="main-heading">
+        <h1 id="heading">Calculator Using React</h1>
+      </div>
+      <div className="calculator">
+        <div className="output">
+          <input type="text" name="" id="" placeholder={val} readOnly />
+        </div>
+        <div className="calculator-btns">
+          <div className="btn-row row1">
+            <button onClick={() => setData(7)}>7</button>
+            <button onClick={() => setData(8)}>8</button>
+            <button onClick={() => setData(9)}>9</button>
+            <button onClick={() => setData("+")}>+</button>
+          </div>
+          <div className="btn-row row2">
+            <button onClick={() => setData(4)}>4</button>
+            <button onClick={() => setData(5)}>5</button>
+            <button onClick={() => setData(6)}>6</button>
+            <button onClick={() => setData("-")}>-</button>
+          </div>
+          <div className="btn-row row3">
+            <button onClick={() => setData(1)}>1</button>
+            <button onClick={() => setData(2)}>2</button>
+            <button onClick={() => setData(3)}>3</button>
+            <button onClick={() => setData("*")}>x</button>
+          </div>
+          <div className="btn-row row4">
+            <button className="red" onClick={() => setData("C")}>C</button>
+            <button onClick={() => setData(0)}>0</button>
+            <button onClick={() => setData("=")}>=</button>
+            <button onClick={() => setData("/")}>÷</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
